c21978f046f5b6443c4f355a2754508d2aec5be7
jordom <jordanmoffatt@brachiosaur.local>
2017-03-22 21:17:27 -0400

    declared vars

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*
+Declare variables:
+masthead (HTML container storing
+the current slide - slide-show part)
+
+slides (array of elements storing
+all slides - slide-show part)
+
+buttons (collected list of
+3 buttons in array - main content hide/reveal part)
+
+contents (array of 3 elements - contents
+in main content hide/reveal part)
+
+container (HTML element that will get a content
+from array contents)
+
+i (variable to be used as index-pointer
+inside loops for slide-show part)
+
+k (variable to be used as index-pointer
+inside loops for hide/reveal content part) */
+
+var masthead, slides, buttons, contents, container, i, k;
