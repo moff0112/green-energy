34110c5ac7d71bce7ad67ce121b392612d796298
jordom <jordanmoffatt@brachiosaur.local>
2017-04-02 15:26:38 -0400

    added the slideshow

--- js/main.js
+++ js/main.js
@@ -21,149 +21,20 @@ inside loops for slide-show part)
 k (variable to be used as index-pointer
 inside loops for hide/reveal content part) */
 
-var masthead, myIndex, slides, buttons, contents, container, i, k;
+var y, i, masthead;
 
-/*
-Access empty div with the
-class "mh-image" (look at the HTML structure
-above), and pass the reference
-to variable masthead */
+y = 0;
 
-masthead = getElementsByClassName("mySlides");
-
-/*
-Assign 4 elements to array slides
-(slide-show content) */
-
-slides = [
-  "<img src='./img/nuclear.jpg'>",
-  "<img src='./img/nuclear.jpg'>",
-  "<img src='./img/nuclear.jpg'>",
-]
-
-
-
-/*
-Define function fadeOut. This function will
-make currently displayed slide disappear. */
-   /*
-   Use style object with property opacity
-   on masthead and set it to 0. */
-/*
-End function fadeOut. */
-
-/*
-Use i as index pointer for the array slides.
-Assign i with initial value 0. */
-
+masthead = document.getElementsByClassName("slides");
 
 runSlides();
 
 function runSlides() {
-  for (i = 0; i < masthead.length; i += 1) {
-    masthead[i].style.display = "none";
-  }
-
-  myIndex++;
-    if (myIndex > masthead.length) {myIndex = 1}
-    masthead[myIndex-1].styledisplay = "block";
-    setTimeOut(runSlides, 5000);
+    for (i = 0; i < masthead.length; i += 1) {
+       masthead[i].style.display = "none";
+    }
+    y += 1;
+    if (y > masthead.length) {y = 1}
+    masthead[y-1].style.display = "block";
+    setTimeout(runSlides, 2000);
 }
-
-
-}
-
-/*
-Define function runSlides.
-This function will loop through the array slides. */
-   /*
-   Set opacity of masthead to 1.
-   Make sure masthead is visible
-   (use style object and opacity on masthead). */
-
-   /*
-   Check the value of i.
-   If the value of i is greater than 3
-   (3 is index of fourth slide),
-   reset the value of i to 0
-   (slide-show starts over). */
-
-   /*
-   Use i to pass the next in line slide
-   from array slides to masthead. */
-
-   /*
-   Increment i. */
-
-   /*
-   Use setTimeOut() to call function fadeout
-   after 5 seconds.(Slide will remain displayed
-   for 5s and then, function fadeOut
-   will make it disappear). */
-/*
-End function runSlides. */
-
-/*
-Call function runSlides. This will display
-the first slide as the web page is loaded. */
-
-/*
-Use setInterval() to call function
-runSlides every 6 seconds.
-(Function runSlides is
-a slide-show "engine"). */
-
-
-/*
--------------------------------------
-START CODING REVEAL-HIDE MAIN-CONTENT
--------------------------------------
-HTML Structure:
-<main>
-  <div class="box">
-    <h1>Heading ...</h1>
-    <ul>
-      <li><span class="btn" id="active">Chrome</span></li>
-      <li><span class="btn">Firefox</span></li>
-      <li><span class="btn">Opera</span></li>
-    </ul>
-    <div class="container"></div>
-  </div>
-</main> */
-
-
-/*
-Collect all buttons in array buttons.
-
-Have all contents in array contents.
-
-Access div with the class "container"
-and pass the reference to variable container.*/
-
-/*
-Pass the value of the first array-element of array contents
-to container (use innerHTML on container). */
-
-/*
-Define function handleClick
-(pass event object to function) */
-   /*
-   Loop through buttons array and
-   use removeAttribute() to remove
-   id with the value active from an HTML element
-   that contains this id (use hasAttribute() in if-statement to check). */
-
-   /*
-   Use innerHTML on ev.target to check which button
-   was clicked. Based on this, display corresponding array
-   element inside <div class="container"></div> */
-
-   /*
-   Add the class active to clicked list item (use ev.target) */
-/*
-End function handleClick. */
-
-/*
-Loop through buttons array and
-register handleClick to listen for "click" event
-on any button from array buttons. */
