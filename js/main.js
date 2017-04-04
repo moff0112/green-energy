119763b97da7546822e5ee80707c6f4f0020c906
jordom <jordanmoffatt@brachiosaur.local>
2017-04-03 21:12:23 -0400

    added images

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@ function runSlides() {
 
 buttons = document.getElementsByClassName("btn");
 
-contents = ["<p>This is content one<p>",
-            "<p>This is content two</p>",
-            "<p>This is content three</p>"
+contents = ["<p>One Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>",
+            "<p>Two Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>",
+            "<p>Three dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>"
           ];
 
 imge = ["<img src='./img/hydro.jpg'/>",
@@ -33,6 +33,8 @@ imge = ["<img src='./img/hydro.jpg'/>",
 container = document.getElementsByClassName("container")[0];
 
 photo = document.getElementsByClassName("slider")[0];
+
+container.innerHTML = contents[0];
 
 function handleClick(ev) {
   ev.preventDefault();
