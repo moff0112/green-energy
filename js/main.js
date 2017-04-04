73a2dae487645f3a3fc9b26ec00e621530aecf7f
jordom <jordanmoffatt@brachiosaur.local>
2017-04-03 21:48:16 -0400

    fixed icons

--- js/main.js
+++ js/main.js
@@ -8,25 +8,27 @@ runSlides();
 
 function runSlides() {
     for (i = 0; i < masthead.length; i += 1) {
-       masthead[i].style.display = "none";
+        masthead[i].style.display = "none";
     }
     y += 1;
-    if (y > masthead.length) {y = 1}
-    masthead[y-1].style.display = "block";
+    if (y > masthead.length) {
+        y = 1
+    }
+    masthead[y - 1].style.display = "block";
     setTimeout(runSlides, 6000);
 }
 
 
-buttons = document.getElementsByClassName("btn");
+buttons = document.querySelectorAll(".icons span");
 
 contents = ["<p>One Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>",
-            "<p>Two Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>",
-            "<p>Three dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>"
-          ];
+    "<p>Two Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>",
+    "<p>Three dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>"
+];
 
-imge = ["<img src='./img/hydro.jpg'/>",
-        "<img src='./img/hydro.jpg'/>",
-        "<img src='./img/hydro.jpg'/>"
+imge = ["<img src='./img/bicycle.jpg'/>",
+    "<img src='./img/bulb.jpg'/>",
+    "<img src='./img/veggies.jpg'/>"
 ];
 
 
@@ -35,24 +37,25 @@ container = document.getElementsByClassName("container")[0];
 photo = document.getElementsByClassName("slider")[0];
 
 container.innerHTML = contents[0];
+photo.innerHTML = imge[0];
 
 function handleClick(ev) {
-  ev.preventDefault();
+    ev.preventDefault();
 
-  for (i = 0; i < buttons.length; i += 1) {
-    if (buttons[i].hasAttribute("id")) {
-        buttons[i].removeAttribute("id");
+    for (i = 0; i < buttons.length; i += 1) {
+        if (buttons[i].hasAttribute("id")) {
+            buttons[i].removeAttribute("id");
+        }
+
+        if (ev.target === buttons[i]) {
+            container.innerHTML = contents[i];
+            photo.innerHTML = imge[i];
+            buttons[i].setAttribute("id", "active");
+        }
+
     }
-
-    if (ev.target === buttons[i]) {
-      container.innerHTML = contents[i];
-      photo.innerHTML = imge[i];
-      buttons[i].setAttribute("id", "active");
-    }
-
-  }
 }
 
 for (i = 0; i < buttons.length; i += 1) {
-  buttons[i].addEventListener("click", handleClick, false);
+    buttons[i].addEventListener("click", handleClick, false);
 }
